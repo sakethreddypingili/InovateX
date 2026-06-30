61b7d204c59b097f95097a3e2c0f017b82314e33
saketh reddy pingili <pingilisakethreddy@gmail.com>
2026-06-30 13:23:40 +0530

    feat: toggle layout theme css utility classes

---
+++ JS/state.js
@@ -0,0 +1,25 @@
+const members = [
+    {
+        photo : "assets/images/shivanshu.png",
+        name : "Shivanshu Shukla" , 
+        role : "Backend & DevOps" , 
+        Contributions : ["Portfolio"]  ,
+        github : "https://github.com/shivanshushukla2602-hash"
+    } , 
+    {
+        photo : "assets/images/hasini.png",
+        name : "Batchu Lakshmi Hasini" , 
+        role : "UI/UX Designer" , 
+        github : "https://github.com/LakshmiHasini"
+    } , 
+    {
+        photo : "assets/images/saketh.png",
+        name : "Pingili Saketh Reddy" , 
+        role : "Backend Developer" , 
+        github : "https://github.com/sakethreddypingili"
+    } , 
+    {
+        photo : "assets/images/satyam.png",
+        name : "Satyam Kumar Jha" , 
+        role : "Frontend Developer" , 
+        github : "https://github.com/satyamkjha001-cpu"
