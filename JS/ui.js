a63e3cda3357d3c192d459f034109fcb013a622f
shivanshu19 <shivanshushukla2602@gmail.com>
2026-06-30 14:01:07 +0530

    feat: run validation checks on form message inputs text

--- JS/ui.js
+++ JS/ui.js
@@ -30,3 +30,18 @@ const contactForm = document.querySelector("#contactForm");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message");
+const nameMsg = document.querySelector("#nameMsg");
+const emailMsg = document.querySelector("#emailMsg");
+const textareaMsg = document.querySelector("#textareaMsg");
+const emailHelp = document.querySelector("#emailHelp");
+
+const submitBtn = document.querySelector("#submitBtn");
+
+submitBtn.addEventListener("click" , function(event){
+
+    event.preventDefault();
+
+    // Flag ---> check karne ke liye agar ek bhi invalid nikla to hum input field ka data erase nahi karenge
+    let isValid = true;
+
+    let name = nameInput.value.trim();
